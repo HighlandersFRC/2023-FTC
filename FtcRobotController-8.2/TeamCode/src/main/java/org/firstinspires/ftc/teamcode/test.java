5f8ffbcac785a0b67f897b4b7c4b5e5a56e057ec
ethanlululu <90187@psdchool.org>
2023-08-30 19:04:12 -0600

    It can drive it might be able to move the arm

--- FtcRobotController-8.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
+++ FtcRobotController-8.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
@@ -6,10 +6,12 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Gyroscope;
 
 import com.qualcomm.robotcore.hardware.Servo;
@@ -20,7 +22,7 @@ import com.qualcomm.robotcore.hardware.TouchSensor;
 
 public class test extends LinearOpMode {
     //Initialise motors, classes, and sensors here
-
+    private DcMotor Arm;
     private DcMotor Left_Front;
     private DcMotor Right_Front;
     private DcMotor Left_Back;
@@ -33,19 +35,48 @@ public class test extends LinearOpMode {
         Right_Front = hardwareMap.dcMotor.get("Right_Front");
         Left_Back = hardwareMap.dcMotor.get("Left_Back");
         Right_Back = hardwareMap.dcMotor.get("Right_Back");
-// Wait for the game to start (driver presses PLAY)
+        Arm = hardwareMap.dcMotor.get("Arm");
+        // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
 // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
             double LeftY = gamepad1.left_stick_y;
             double LeftX = gamepad1.left_stick_x;
-            Left_Front.setPower(LeftY);
-            Right_Front.setPower(-LeftY);
-            Left_Back.setPower(LeftY);
-            Right_Back.setPower(-LeftY);
-            Left_Front.setPower(LeftX);
-            Right_Back.setPower(LeftX);
+            double vertical;
+            double horizontal;
+            double pivot;
+            double arm;
+            vertical = -gamepad1.left_stick_y;
+            horizontal = gamepad1.left_stick_x;
+            pivot = -gamepad1.right_stick_x;
+            vertical = -gamepad1.left_stick_y;
+
+            //Mecanum drive
+            Right_Front.setPower(pivot + (vertical + horizontal));
+            Right_Back.setPower(pivot + (vertical - horizontal));
+            Left_Back.setPower(pivot + (-vertical - horizontal));
+            Left_Front.setPower(pivot + (-vertical + horizontal));
+
+            //Code for arm
+
+            if (gamepad1.y) ;
+            {   Arm.setTargetPosition(100);
+                Arm.setPower(-1);
+
+            }
+        }
+            if (gamepad1.a) {
+
+                Arm.setPower(1);
+
+
+            if (gamepad1.b) {
+
+                Arm.setPower(-.5);
+
+            }
         }
     }
 }
+
