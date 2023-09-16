94334b53cdf5f42ef89cd7d48678d2ff2a168647
ethanlululu <90187@psdchool.org>
2023-09-16 11:06:42 -0600

    It can drive and move arm

--- FtcRobotController-8.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ FtcRobotController-8.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -11,20 +11,20 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public class Test extends LinearOpMode {
     //Initialise motors, classes, and sensors here
     private DcMotor Arm1;
-   // private DcMotor Left_Front;
-   // private DcMotor Right_Front;
-   //private DcMotor Left_Back;
-   //private DcMotor Right_Back;
+    private DcMotor Left_Front;
+    private DcMotor Right_Front;
+   private DcMotor Left_Back;
+   private DcMotor Right_Back;
 
     PID1 PID = new PID1();
 
     @Override
 
     public void runOpMode() {
-        //Left_Front = hardwareMap.dcMotor.get("Left_Front");
-        //Right_Front = hardwareMap.dcMotor.get("Right_Front");
-        //Left_Back = hardwareMap.dcMotor.get("Left_Back");
-        //Right_Back = hardwareMap.dcMotor.get("Right_Back");
+        Left_Front = hardwareMap.dcMotor.get("Left_Front");
+        Right_Front = hardwareMap.dcMotor.get("Right_Front");
+        Left_Back = hardwareMap.dcMotor.get("Left_Back");
+        Right_Back = hardwareMap.dcMotor.get("Right_Back");
         Arm1 = hardwareMap.dcMotor.get("Arm1");
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -37,43 +37,48 @@ public class Test extends LinearOpMode {
             double horizontal;
             double pivot;
             double arm1;
-            //vertical = -gamepad1.left_stick_y;
-            //horizontal = gamepad1.left_stick_x;
-            //pivot = -gamepad1.right_stick_x;
-            //vertical = -gamepad1.left_stick_y;
+            vertical = -gamepad1.left_stick_y;
+            horizontal = gamepad1.left_stick_x;
+            pivot = -gamepad1.right_stick_x;
+            vertical = -gamepad1.left_stick_y;
 
             //Mecanum drive
-            //Right_Front.setPower(pivot + (-vertical + horizontal));
-            //Right_Back.setPower(pivot + (-vertical - horizontal));
-            //Left_Back.setPower(pivot + (vertical - horizontal));
-            //Left_Front.setPower(pivot + (vertical + horizontal));
-            //waitForStart();
+            Right_Front.setPower(pivot + (-vertical + horizontal));
+            Right_Back.setPower(pivot + (-vertical - horizontal));
+            Left_Back.setPower(pivot + (vertical - horizontal));
+            Left_Front.setPower(pivot + (vertical + horizontal));
+            waitForStart();
 
 
             //Code for arm
 
             while (opModeIsActive()) {
-                PID.setPID(1.5,.0 ,.0);
-                PID.setSetPoint(-900);
-                PID.updatePID(Arm1.getCurrentPosition());
-                if (gamepad1.a) {
-                    PID.setSetPoint(-900);
-                    PID.updatePID(Arm1.getCurrentPosition());
-                    Arm1.setPower(1);
+                PID.setPID(.75,.2 ,.3);
                     Arm1.setTargetPosition(PID.getSetPoint());
                     Arm1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    if (gamepad1.x) {
+                        PID.setSetPoint(0);
+                        Arm1.setPower(1);
 
-                }
-                if (gamepad1.b) {
+
+                    }
+                    if (gamepad1.b) {
+                        PID.setSetPoint(-3);
+                        PID.updatePID(Arm1.getCurrentPosition());
+                        Arm1.setPower(PID.getResult());
+
+                    }
+                    if(gamepad1.a){
+
+                    }
 
 
-                    Arm1.setPower(.5);
-
-                } else {
-                    Arm1.setPower(0);
-                }
+                    else {
+                        Arm1.setPower(0);
+                    }
                 Arm1.getCurrentPosition();
                 telemetry.addData("Arm Encode", Arm1.getCurrentPosition());
+                telemetry.addData("inches",inchToTicks(Arm1.getCurrentPosition()));
                 telemetry.update();
             }
 
@@ -81,7 +86,7 @@ public class Test extends LinearOpMode {
 
 
         telemetry.addData("Arm Encode", Arm1.getCurrentPosition());
-        telemetry.addData("Arm Inches", ticksToInches(Arm1.getCurrentPosition()));
+        telemetry.addData("Arm Inches", Arm1.getCurrentPosition()/52);
         telemetry.addData("PID Result", PID.getResult());
         telemetry.update();
 
@@ -100,8 +105,6 @@ public class Test extends LinearOpMode {
         return ticks;
     }
 }
-//Arm.setTargetPosition(PID.getSetPoint());
-//            Arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
 
 
