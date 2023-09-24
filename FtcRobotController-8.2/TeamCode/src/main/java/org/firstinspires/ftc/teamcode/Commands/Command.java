b20e017c42914fcda1dfadde82d1d6f89e5b15d3
Advik-highlanders28 <adviksanghi.highlanders@gmail.com>
2023-09-24 11:51:37 -0600

    opencv tests

--- FtcRobotController-8.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/Command.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.Commands;
-
-public abstract class Command {
-    public boolean commandCompleted = false;
-    public abstract void start();
-    public abstract void execute();
-    public abstract void end();
-    public abstract boolean isFinished();
-}
