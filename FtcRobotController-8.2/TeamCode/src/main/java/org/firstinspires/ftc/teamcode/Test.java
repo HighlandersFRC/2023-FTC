b28e0c93ce619f9977f2f9b0add4ebffd5b5fb9d
ethanlululu <90187@psdchool.org>
2023-10-07 18:51:20 -0600

    Pid works i move servos

--- FtcRobotController-8.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ FtcRobotController-8.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -6,13 +6,16 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+
 @TeleOp
 
 public class Test extends LinearOpMode {
-    //Initialise motors, classes, and sensors here
+
+//Initialise motors, classes, and sensors here
+
     private DcMotor Arm1;
-    //private DcMotor Left_Front;
-    //private DcMotor Right_Front;
+   //private DcMotor Left_Front;
+   //private DcMotor Right_Front;
    //private DcMotor Left_Back;
    //private DcMotor Right_Back;
 
@@ -26,88 +29,75 @@ public class Test extends LinearOpMode {
         //Left_Back = hardwareMap.dcMotor.get("Left_Back");
         //Right_Back = hardwareMap.dcMotor.get("Right_Back");
         Arm1 = hardwareMap.dcMotor.get("Arm1");
-        // Wait for the game to start (driver presses PLAY)
+
+    // Wait for the game to start (driver presses PLAY)
+
         waitForStart();
 
-// run until the end of the match (driver presses STOP)
+    // run until the end of the match (driver presses STOP)
+
         while (opModeIsActive()) {
+            //stuff
             double speed;
-            double inchesPerSecond;
-            double vertical;
-            double horizontal;
-            double pivot;
-            double arm1;
-            vertical = -gamepad1.left_stick_y;
-            horizontal = gamepad1.left_stick_x;
+            /*double vertical;
+            //double horizontal;
+            //double pivot;
+            //vertical = -gamepad1.left_stick_y;
+            //horizontal = gamepad1.left_stick_x;
             pivot = -gamepad1.right_stick_x;
-            vertical = -gamepad1.left_stick_y;
+            */
+
             speed = (Arm1.getCurrentPosition()-Arm1.getCurrentPosition())/(time-time);
-            //Mecanum drive
-            //Right_Front.setPower(pivot + (-vertical + horizontal));
+
+    //Mecanum drive
+
+            /*Right_Front.setPower(pivot + (-vertical + horizontal));
             //Right_Back.setPower(pivot + (-vertical - horizontal));
             //Left_Back.setPower(pivot + (vertical - horizontal));
-            //Left_Front.setPower(pivot + (vertical + horizontal));
+            Left_Front.setPower(pivot + (vertical + horizontal));
+            */
             waitForStart();
 
 
-            //Code for arm
+    //Code for arm
 
             while (opModeIsActive()) {
-                PID.setPID(.75,.3 ,.0);
+                PID.setPID(.01,.0 ,.0);
                     Arm1.setTargetPosition(PID.getSetPoint());
                     Arm1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                     if (gamepad1.b) {
-                        PID.setSetPoint(100
-                        );
+                        PID.setSetPoint(1000);
                         Arm1.setPower(1);
-
 
                     }
                     if (gamepad1.x) {
-
-                        PID.setSetPoint(28);
+                        PID.setSetPoint(1000);
                         PID.updatePID(Arm1.getCurrentPosition());
                         Arm1.setPower(PID.getResult());
-
-
-
-
-                    }
-
-
-
+                        telemetry.addData("speed",speed);
+                    }      
                     else {
                         Arm1.setPower(0);
                     }   resetRuntime();
                 Arm1.getCurrentPosition();
 
-                
             }
+
+    //adding data
+
             telemetry.addData("Arm Encode", Arm1.getCurrentPosition());
-            telemetry.addData("Arm Inches", Arm1.getCurrentPosition()/52);
+            telemetry.addData("Arm Inches", ticksToInches(Arm1.getCurrentPosition()));
             telemetry.addData("PID Result", PID.getResult());
+            telemetry.update();
         }
 
-
-
-        telemetry.update();
-
-
-
     }
+
     //units for stuff
+
     public double ticksToInches(double ticks) {
         double inches = ticks / 52;
-        System.out.println(inches);
         return inches;
     }
-    public double inchToTicks(double inches){
-        double ticks = inches * 52;
-        System.out.println(ticks);
-        return ticks;
 
     }
-}
-
-
-
