41dda3c97af65e559abfe2d495ea5d7cdb16899e
Life090701 <090701kequ@gmail.com>
2023-10-20 17:14:34 -0600

    Field centric Mecanum drive with banch of stuff, proto type

--- FtcRobotController-8.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricMecanumTeleOp.java
+++ FtcRobotController-8.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricMecanumTeleOp.java
@@ -42,14 +42,12 @@ public class FieldCentricMecanumTeleOp extends LinearOpMode {
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            double y = -gamepad1.left_stick_y * 0.95; // Remember, Y stick value is reversed
+            double y = -gamepad1.left_stick_y * 0.95;
             double x = gamepad1.left_stick_x * 0.95;
             double rx = -gamepad1.right_stick_x * 0.95;
 
 
-            // This button choice was made so that i   t is hard to hit on accident,
-            // it can be freely changed based on preference.
-            // The equivalent button is start on Xbox-style controllers.
+
             if (gamepad1.options) {
                 imu.resetYaw();
             }
@@ -98,69 +96,6 @@ public class FieldCentricMecanumTeleOp extends LinearOpMode {
 
         }
         else continue;
-        }
-        // Find a motor in the hardware map named "Arm Motor"
-        double CPR = 288;
-
-        int positionLeft_Front = Left_Front.getCurrentPosition();
-        positionLeft_Front = Left_Front.getCurrentPosition();
-        double revolutionsLeft_Front = positionLeft_Front/CPR;
-
-        int positionLeft_Back = Left_Back.getCurrentPosition();
-        positionLeft_Back = Left_Back.getCurrentPosition();
-        double revolutionsLeft_Back = positionLeft_Back/CPR;
-
-        int positionRight_Front = Right_Front.getCurrentPosition();
-        positionRight_Front = Right_Front.getCurrentPosition();
-        double revolutions = positionRight_Front/CPR;
-
-        int positionRight_Back = Right_Back.getCurrentPosition();
-        positionLeft_Back = Right_Back.getCurrentPosition();
-        double revolutionsRight_Back = positionRight_Back/CPR;
-
-        double angle = revolutions * 360;
-        double angleNormalized = angle % 360;
-
-        // Reset the motor encoder so that it reads zero ticks
-
-
-        waitForStart();
-
-        while (opModeIsActive()) {
-
-            // Get the current position of the armMotor
-            positionLeft_Front = Left_Front.getCurrentPosition();
-            positionLeft_Back = Left_Back.getCurrentPosition();
-            positionRight_Front = Right_Front.getCurrentPosition();
-            positionRight_Back = Right_Back.getCurrentPosition();
-
-            // Get the target position of the armMotor
-            double desiredPositionLeft_Front = Left_Front.getTargetPosition();
-            double desiredPositionLeft_Back = Left_Back.getTargetPosition();
-            double desiredPositionRight_Front = Right_Front.getTargetPosition();
-            double desiredPositionRight_Back = Right_Back.getTargetPosition();
-
-
-            // Show the position of the armMotor on telemetry
-            telemetry.addData("Encoder Position Left_Front", Left_Front);
-            telemetry.addData("Encoder Position Left_Back", Left_Back);
-            telemetry.addData("Encoder Position Right_Front", Right_Front);
-            telemetry.addData("Encoder Position Right_Back", Right_Back);
-
-            // Show the target position of the armMotor on telemetry
-            telemetry.addData("Desired Position Left_Front", desiredPositionLeft_Front);
-            telemetry.addData("Desired Position Left_Back", desiredPositionLeft_Back);
-            telemetry.addData("Desired Position Right_Front", desiredPositionRight_Front);
-            telemetry.addData("Desired Position Right_Back", desiredPositionRight_Back);
-
-            telemetry.addData("position Left_Front", positionLeft_Front);
-            telemetry.addData("position Left_Back", positionLeft_Back);
-            telemetry.addData("position Right_Front", positionRight_Front);
-            telemetry.addData("position Right_Back", positionRight_Back);
-
-            telemetry.addData("angle", angle);
-            telemetry.addData("angleNormalized", angleNormalized);
-            telemetry.update();
         }
     }
 }
