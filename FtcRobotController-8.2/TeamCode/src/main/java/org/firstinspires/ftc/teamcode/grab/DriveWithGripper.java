03329414a24bc58333648c088f1cd654056e7fde
ethanlululu <90187@psdschools.org>
2023-10-20 20:21:17 -0600

    wrist works and claw does not

--- FtcRobotController-8.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/grab/DriveWithGripper.java
+++ FtcRobotController-8.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/grab/DriveWithGripper.java
@@ -2,59 +2,60 @@ package org.firstinspires.ftc.teamcode.grab;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.Servo;
-
 
 @TeleOp(name = "Drive Gripper", group = "Exercises")
 //@Disabled
     public class DriveWithGripper extends LinearOpMode {
 
         Servo armServo;
-        CRServo contServo;
+        Servo wristServo;
 
         double contPower;
 
-        //PID1 PID = new PID1(1,1,0);
+
         // called when init button is  pressed.
+
         @Override
+
         public void runOpMode() throws InterruptedException {
-            contServo = hardwareMap.crservo.get("contServo");
+            wristServo = hardwareMap.servo.get("wristServo");
             armServo = hardwareMap.servo.get("armServo");
             telemetry.addData("Mode", "waiting");
+
             telemetry.update();
+
 
             // wait for start button.
 
             waitForStart();
 
             while (opModeIsActive()) {
+                wristServo.scaleRange(0,0.1);
+                telemetry.addData("time",getRuntime());
 
-
+                telemetry.addData("ArmPosition",wristServo.getPosition());
                 telemetry.addData("Mode", "running");
 
                 if (gamepad1.x) {
 
-                    contPower =1;
-                    contServo.setPower(contPower);
+                    wristServo.setPosition(1);
                 }
                 if(gamepad1.y) {
-                    contPower=-1;
 
+                    wristServo.setPosition(.001);
                 }
                 if (gamepad1.a){
                     armServo.setPosition(0);
                 }
                     if (gamepad1.b) {
-                    armServo.setPosition(220);
+                        armServo.setDirection(Servo.Direction.FORWARD);
+                        armServo.setPosition(110);
+
+                    }
+                else {
+
                 }
-                else
-                    contPower = 0.0;
-
-
-
-
-
                 telemetry.update();
                 idle();
             }
