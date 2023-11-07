909c3a3dd2a4f2c58c2394b732154521d15544f2
Rominemaster <ethan.romine17@gmail.co>
2023-11-06 18:59:32 -0700

    Mecanum Drive Working

--- FtcRobotController-8.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTest.java
+++ FtcRobotController-8.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTest.java
@@ -3,28 +3,36 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
 
 @TeleOp
 public class MecanumTest extends LinearOpMode {
+    //device variables
     public DcMotor Left_Back;
     public DcMotor Right_Back;
     public DcMotor Left_Front;
     public DcMotor Right_Front;
+    public IMU imu;
     @Override
     public void runOpMode() {
 
         waitForStart();
-        double leftStickX = gamepad1.left_stick_x;
-        double leftStickY = -gamepad1.left_stick_y;
-        double rightStickX = gamepad1.right_stick_x;
 
+        //hardware mapping
         Left_Front = hardwareMap.dcMotor.get("Left_Front");
         Right_Front = hardwareMap.dcMotor.get("Right_Front");
         Left_Back = hardwareMap.dcMotor.get("Left_Back");
         Right_Back = hardwareMap.dcMotor.get("Right_Back");
+        imu = hardwareMap.get(IMU.class, "imu");
 
         while (opModeIsActive()) {
 
+            //getting needed inputs
+            double leftStickX = gamepad1.left_stick_x;
+            double leftStickY = -gamepad1.left_stick_y;
+            double rightStickX = gamepad1.right_stick_x;
+
+            //preventing random drifting
             if (Math.abs(leftStickX) < 0.0001){
                 Left_Front.setPower(0);
                 Left_Back.setPower(0);
@@ -38,23 +46,35 @@ public class MecanumTest extends LinearOpMode {
                 Right_Back.setPower(0);
             }
 
+            //getting input
             double y = leftStickY;
             double x = leftStickX;
             double rx = rightStickX;
 
+            //calculating power
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
 
-            double frontLeftPower = (y + x + rx);
-            double backLeftPower = (y - x + rx);
-            double frontRightPower = (y - x - rx);
-            double backRightPower = (y + x - rx);
-
-            Left_Front.setPower(frontLeftPower);
-            Left_Back.setPower(backLeftPower);
+            //setting power
+            Left_Front.setPower(-frontLeftPower);
+            Left_Back.setPower(-backLeftPower);
             Right_Front.setPower(frontRightPower);
             Right_Back.setPower(backRightPower);
 
+            //debug
+            telemetry.addData("Front Left Power", frontLeftPower);
+            telemetry.addData("Back Left Power", backLeftPower);
+            telemetry.addData("Front Right Power", frontRightPower);
+            telemetry.addData("Back Right Power", backRightPower);
 
+            telemetry.addData("Left Stick X", x);
+            telemetry.addData("Left Stick Y", y);
+            telemetry.addData("Right Stick X", rx);
 
+            telemetry.update();
         }
 
 
